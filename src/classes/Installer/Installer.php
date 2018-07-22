a46b7652ee91107bf9de667dddf123e9afd8ac8e
Ammar Faizi <ammarfaizi2@gmail.com>
2018-07-22 16:47:46 +0700

    Update installer

--- src/classes/Installer/Installer.php
+++ src/classes/Installer/Installer.php
@@ -2,14 +2,38 @@
 
 namespace Installer;
 
+use Installer\Linux\LinuxInstaller;
+
 /**
  * @author Amamr Faizi <ammarfaizi2@gmail.com>
  * @version 0.0.1
+ * @license MIT
+ * @since 0.0.1
  */
 class Installer
 {
+	/**
+	 * @var \Installer\Linux\LinuxInstaller
+	 */
+	private $installer;
+
+	/**
+	 * Constructor.
+	 */
 	public function __construct()
 	{
+		if (PHP_OS === "Linux") {
+			$this->installer = new LinuxInstaller;
+		}
+	}
 
+	/**
+	 * @param string $method
+	 * @param array  $parameters
+	 * @return mixed
+	 */
+	public function __call($method, $parameters)
+	{
+		return call_user_func_array([$this->installer, $method], $parameters);
 	}
 }
---
+++ src/classes/Installer/Linux/LinuxInstaller.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Installer\Linux;
+
+use Installer\Linux\LinuxInstaller;
+
+/**
+ * @author Amamr Faizi <ammarfaizi2@gmail.com>
+ * @version 0.0.1
+ * @license MIT
+ * @since 0.0.1
+ */
+class LinuxInstaller
+{
+	/**
+	 * Constructor.
+	 */
+	public function __construct()
+	{
+		$argv = $_SERVER["argv"];
+	}
+
+	public function isInstalled()
+	{
+		
+	}
+}

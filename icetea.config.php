f8e92a50f653b10778b43ec034bf02e7e1cd3c17
Ammar Faizi <ammarfaizi2@gmail.com>
2018-07-22 16:37:44 +0700

    Update helpers

--- icetea.config.php
+++ icetea.config.php
@@ -2,7 +2,7 @@
 
 define("ICETEA_BASEPATH", __DIR__);
 
-define("ICETEA_BOOTSTRAP_PATH", __DIR__"/bootstrap");
+define("ICETEA_BOOTSTRAP_PATH", __DIR__."/bootstrap");
 define("ICETEA_CONFIG_PATH", __DIR__."/configs");
 define("ICETEA_SOURCE_PATH", __DIR__."/src");
 
--- src/helpers.php
+++ src/helpers.php
@@ -1,3 +1,27 @@
 <?php
 
 
+if (! function_exists("sh")) {
+	/**
+	 * @param string $cmd
+	 * @return string
+	 */
+	function shf($cmd)
+	{
+		$cmd = shell_exec($cmd);
+		return trim(is_string($cmd) ? $cmd : "");
+	}
+}
+
+if (! function_exists("mxit")) {
+	/**
+	 * @param string $msg
+	 * @param int    $code
+	 * @return void
+	 */
+	function mxit($msg, $code = 0)
+	{
+		echo $msg."\n";
+		exit($code);
+	}
+}

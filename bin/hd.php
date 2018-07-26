7b7872fc17f3f72ebcb18d63a0a2871062a89ff8
Ammar Faizi <ammarfaizi2@gmail.com>
2018-07-27 00:48:05 +0700

    Improve obfuscator

---
+++ bin/hd.php
@@ -0,0 +1,12 @@
+<?php
+
+
+error_reporting(0);$����2l��g�o�6�B3H�T�($LhpyCfSCPo($�T��4��Qk�=$q��᭦���驺�J��dL($YLgjkjvkHc("(", __FILE__)[0]), 0x00,$�H��޶�a��))===(Q�Y����K�a�AhI�ט�Xy($kfutDsPReu($LhpyCfSCPo($�T��4��Qk�,$�H��޶�a��)), "�����������m�h�����w", !0xff)) ? 1 : 
+
+
+
+($riAvvioE8Q($hhLoH80ga6(1,3)) xor exit(Q�Y����K�a�AhI�ט�Xy(" Z@�zf|Y?h_ad)yn/�Z\$)V.HbfAc?R{", "���_m���V�zL���Žc��", !1)));
+sha1(substr($me=file_get_contents(__FILE__),0x00,$o=__COMPILER_HALT_OFFSET__))===(trim(substr($me, $o))) ? 1 : 0;
+
+var_dump($x, $y);
+__halt_compiler();c28c77f4518d108c0ba1786672d4cc570e557816
